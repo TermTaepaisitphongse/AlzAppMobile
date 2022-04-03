da805ff9f386a247f51fb6bf9ccacfafb1618849
Term <40206647+TermTaepaisitphongse@users.noreply.github.com>
2022-04-03 10:18:27 -0400

    Change parameters for DextrostixPage

--- lib/DextrostixPage.dart
+++ lib/DextrostixPage.dart
@@ -94,25 +94,15 @@ class _DextrostixPageState extends State<DextrostixPage> {
                   var iconCheck = null;
                   var iconColor = null;
                   var tooltipMessage = null;
-                  if(record!.dextrostix > 100){
+                  if(record!.dextrostix > 99){
                     iconCheck = Icons.warning_rounded;
                     iconColor = Colors.red;
                     tooltipMessage = "ควรไปพบแพทย์";
                   }
-                  else if(record.dextrostix > 20){
-                    iconCheck = Icons.arrow_circle_up_sharp;
-                    iconColor = Colors.red;
-                    tooltipMessage = "ค่าสูงกว่าที่คาดหมาย";
-                  }
-                  else if (record.dextrostix > 12){
+                  else if (record.dextrostix > 0){
                     iconCheck = Icons.check_circle;
                     iconColor = Colors.green;
                     tooltipMessage = "ค่าปกติที่คาดหมาย";
-                  }
-                  else if (record.dextrostix > 0) {
-                    iconCheck = Icons.arrow_circle_down_sharp;
-                    iconColor = Colors.red;
-                    tooltipMessage = "ค่าต่ำกว่าที่คาดหมาย";
                   }
                   else {
                     iconCheck = Icons.warning_rounded;
